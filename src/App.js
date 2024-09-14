d11dc8fa55b21b370ac86a21f243d43040eb8e92
Shorya Malani <shoryamal@gmail.com>
2024-09-14 05:36:58 -0400

    trying new front end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import './output.css';
 import { useState, useEffect } from 'react';
 
 function PlayerCard({ name }) {
   return (
-    <div className="col-span-1 bg-blue-500 p-4 m-2 rounded-md">
+    <div className=" player-card col-span-1 bg-blue-500 p-4 m-2 drop-shadow-[0_15px_15px_rgba(185,185,185,.25)] ">
       <div>{name}</div>
       <div>Performance stats</div>
     </div>
@@ -14,33 +14,33 @@ function PlayerCard({ name }) {
 function Game( { gameID, inGameSetter }) {
   const [gameState, setGameState] = useState(gameID); //TODO more state info?
 
-  useEffect(() => {
-    fetch('https://gaze.shoryamalani.com/api/getGameData')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setGameState(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://gaze.shoryamalani.com/api/getGameData')
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setGameState(data);
+  //     });
+  // }, []);
 
   return (
     <>
-      <div className="col-span-3 p-4 grid-cols-subgrid">
+      <div className="col-span-3 p-4 grid-cols-subgrid ">
         <button onClick={e => inGameSetter(
           {playing: false, game: 0, player: "new player name"} //TODO username generation api call?
-        )} className="bg-red-900 p-4 rounded-md">Exit</button>
+        )} className="bg-red-900 p-4 drop-shadow-[0_15px_15px_rgba(185,185,185,.25)]">Exit</button>
       </div>
       
-      <PlayerCard name="User Info" />
-      <div className='col-span-1' />
-      <PlayerCard name="Saucy Asparagus" />
+      <PlayerCard name="User Info"class="player-card" />
+      <div className='col-span-1 ' />
+      <PlayerCard name="Saucy Asparagus" class="saucy-asparagus" />
       <PlayerCard name="Confused Carrot" />
       <div className='col-span-1' />
       <PlayerCard name="Wacky Watermelon" />
-      {gameState.users.map((u) => (
+      {/* {gameState.users.map((u) => (
         <PlayerCard name={u} /> // TODO Generate player stat cards from gamestate users
-      ))}
+      ))} */}
     </>
   );
 }
@@ -49,7 +49,8 @@ function GameMenu({ inGameSetter }) {
   const [games, setGames] = useState([]);
 
   useEffect(() => {
-    fetch('https://gaze.shoryamalani.com/api/getActiveGames')
+    var url = '/api/getActiveGames';
+    fetch(url)
       .then((res) => {
         return res.json();
       })
@@ -59,24 +60,24 @@ function GameMenu({ inGameSetter }) {
   }, []);
 
   function CreateGameButton() {
-    return <button className="bg-green-700 p-4 rounded-md" onClick={e => {
-      fetch('https://gaze.shoryamalani.com/api/createGame', {
-        method: "post",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          user_id: 1, // TODO where are we pulling this from
-        })
-      })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        inGameSetter({playing: true, game: data.game, player: data.player}); // TODO fix data
-      });
-    }}></button>
+    return <button className="bg-red-900 p-4 p-4" onClick={e => {
+      // fetch('https://gaze.shoryamalani.com/api/createGame', {
+      //   method: "post",
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify({
+      //     user_id: 1, // TODO where are we pulling this from
+      //   })
+      // })
+      // .then((res) => {
+      //   return res.json();
+      // })
+      // .then((data) => {
+      //   inGameSetter({playing: true, game: data.game, player: data.player}); // TODO fix data
+      // });
+    }}>Create Game</button>
   }
 
   return (
@@ -84,23 +85,23 @@ function GameMenu({ inGameSetter }) {
       <div>
         <CreateGameButton />
         {games.map((g) => (
-          <button className="bg-green-600 p-4 rounded-md" onClick={e => {
-            fetch('https://gaze.shoryamalani.com/api/joinGame', {
-              method: "post",
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                joinCode: g,
-              })
-            })
-            .then((res) => {
-              return res.json();
-            })
-            .then((data) => {
-              inGameSetter({playing: true, game: data.game, player: data.player}); // TODO fix data
-            });
+          <button className="bg-red-900 p-4 p-4" onClick={e => {
+            // fetch('https://gaze.shoryamalani.com/api/joinGame', {
+            //   method: "post",
+            //   headers: {
+            //     'Accept': 'application/json',
+            //     'Content-Type': 'application/json'
+            //   },
+            //   body: JSON.stringify({
+            //     joinCode: g,
+            //   })
+            // })
+            // .then((res) => {
+            //   return res.json();
+            // })
+            // .then((data) => {
+            //   inGameSetter({playing: true, game: data.game, player: data.player}); // TODO fix data
+            // });
           }}>Enter Game {g}</button>
         ))}
       </div>
@@ -111,11 +112,11 @@ function GameMenu({ inGameSetter }) {
 
 function GameManager () {
   const [inGame, setInGame] = useState({playing: false, game: 0, player: "new"});
-  useEffect(() => {
-    fetch('https://gaze.shoryamalani.com/api/joinGame')
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://gaze.shoryamalani.com/api/joinGame')
+  // }, []);
 
-  if (inGame) {
+  if (inGame.playing) {
     return <Game gameID={inGame.game} inGameSetter={setInGame}/>
   }
   else {
@@ -125,7 +126,7 @@ function GameManager () {
 
 function App() {
   return (
-    <div className="p-12 bg-slate-800 text-slate-200 grid grid-cols-3">
+    <div className="p-12  text-slate-200 grid grid-cols-3">
       <GameManager />
     </div>
   );
