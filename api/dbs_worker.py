912c610463cd2fb38914c21d579425fdc2cecb30
Shorya Malani <shoryamal@gmail.com>
2024-09-14 05:40:58 -0400

    trying new front end

--- api/dbs_worker.py
+++ api/dbs_worker.py
@@ -110,7 +110,7 @@ def get_active_games(conn):
     games = cur.fetchall()
     final_games = []
     for game in games:
-        if get_game_users(conn, game[0]):
+        if len(get_game_users(conn, game[0])):
             final_games.append(game)
     return games
 
